96da56706056b4d99de2e2c06f4c0e2e66531793
manju_m <mcmanju83@gmail.com>
2021-06-09 09:31:34 +0530

    Updated icons and Checking network

---
+++ app/src/main/java/com/sample/xomeassesment/NetworkUtil.kt
@@ -0,0 +1,30 @@
+package com.sample.xomeassesment
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+var isNetworkAvailableOverride: Boolean? = null
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun isNetworkAvailable(context: Context): Boolean {
+    return isNetworkAvailableOverride?.let {
+        isNetworkAvailableOverride
+    } ?: run {
+        val cm =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        cm.activeNetwork?.let { n ->
+            cm.getNetworkCapabilities(n)?.let { nc ->
+                return nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(
+                    NetworkCapabilities.TRANSPORT_WIFI
+                ) || nc.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
+            }
+        }
+
+        return false
+    }
+}
+
--- app/src/main/java/com/sample/xomeassesment/view/FlickrPhotoActivity.kt
+++ app/src/main/java/com/sample/xomeassesment/view/FlickrPhotoActivity.kt
@@ -1,13 +1,16 @@
 package com.sample.xomeassesment.view
 
+import android.os.Build
 import android.os.Bundle
 import android.widget.SearchView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.observe
 import com.sample.xomeassesment.R
 import com.sample.xomeassesment.databinding.ActivityMainBinding
+import com.sample.xomeassesment.isNetworkAvailable
 import com.sample.xomeassesment.viewmodel.FlickrPhotoViewModel
 
 class FlickrPhotoActivity : AppCompatActivity() {
@@ -30,19 +33,24 @@ class FlickrPhotoActivity : AppCompatActivity() {
         })
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun getData(name: String) {
-        flickrPhotoViewModel.getSearchImages(name).observe(
-            this,
-            {
-                it.onSuccess {
-                    it?.photos?.photo?.let {
-                        binding?.rvImages?.adapter = FlickrPhotoAdapter(it)
+        if (isNetworkAvailable(this)) {
+            flickrPhotoViewModel.getSearchImages(name).observe(
+                this,
+                {
+                    it.onSuccess {
+                        it?.photos?.photo?.let {
+                            binding?.rvImages?.adapter = FlickrPhotoAdapter(it)
+                        }
+                    }
+                    it.onFailure {
+                        Toast.makeText(this, R.string.no_data_found, Toast.LENGTH_SHORT).show()
                     }
                 }
-                it.onFailure {
-                    Toast.makeText(this, R.string.no_data_found, Toast.LENGTH_SHORT).show()
-                }
-            }
-        )
+            )
+        } else {
+            Toast.makeText(this, R.string.network_issue, Toast.LENGTH_SHORT).show()
+        }
     }
 }
